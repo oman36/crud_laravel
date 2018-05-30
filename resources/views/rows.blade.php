b2201f2d4098c8e09f8412a093b71f4c94f533e1
Vladimir Petrov <vladimir@radushin.com>
2018-05-30 15:43:31 +0400

    Hidding filters was fixed

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -73,7 +73,7 @@
                 e.preventDefault();
             }
         });
-        if ({{array_reduce(request('filters'), function ($item, $before) { return $item || $before;})}}) {
+        if ({{array_reduce(request('filters', []), function ($item, $before) { return $item || $before;})}}) {
             $('a[href="#collapseExample"]').click()
         }
     </script>
